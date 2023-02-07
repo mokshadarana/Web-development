02d85fe8cdd01ed621e8e07a1a01e86e7dcb0460
mokshadarana <np03cs4a220359@heraldcollege.edu.np>
2023-02-07 23:13:46 +0545

    Added more html and css

--- javascript1/trial.js
+++ javascript1/trial.js
@@ -1,8 +1,10 @@
-{
-let country = 'Nepal';
-let continent = 'Asia';
-let population = 30.03;
-console.log(country);
-console.log(continent);
-console.log(population);
+const percentageOfWorld1= function(population){
+    return(population/7900)*100;
+};
+const populations = [10, 1441, 332, 83];
+const percentages2 = [];
+for (let i = 0; i < populations.length; i++) {
+const perc = percentageOfWorld1(populations[i]);
+percentages2.push(perc);
 }
+console.log(percentages2);
